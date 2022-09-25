55313b18c2b291b597c5a0321bc9b14fbc1b6cac
이희범 <71597554+lhb00@users.noreply.github.com>
2022-09-25 21:40:48 +0900

    Delete Assignment 1.ts

--- Assignment 1.ts
+++
@@ -1,9 +0,0 @@
-type Last = <T>(arr:T[]) => T
-
-type Prepend = <M>(arr:M[], item: M) => M
-
-const last : Last = (arr) => arr[arr.length -1]
-const prepend : Prepend = (arr, item) => {
-    arr.unshift(item)
-    return arr[0]
-}
